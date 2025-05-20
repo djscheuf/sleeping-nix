d7f92608586b0d00b67cd7ec3afb5d37601a5737
Daniel Scheufler <djscheufler@gmail.com>
2025-05-20 08:49:02 -0500

    feat(auto-upgrading): renames gc to os-mgmt, added auto-upgrade settings.

--- Framework/os-mgmt.nix
+++ Framework/os-mgmt.nix
@@ -8,4 +8,7 @@
 		options = "--delete-older-than 14d"; # anything older than 2 weeks.
 		# should never delete current generation.
 	};	
+
+	system.autoUpgrade.enable = true;
+	system.autoUpgrade.channel = "https://channels.nixos.org/nixos-25.05";
 }
