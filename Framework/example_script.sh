94d3c5cd319de2bf360cc200e5fd5f575c6f1524
Daniel Scheufler <djscheufler@gmail.com>
2025-11-18 11:06:15 -0600

    doc(bash script adjustments)

---
+++ Framework/example_script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This is an example script
+# It can be run with `./example_script.sh`
+# It shows the adjustment needed to make a script work in a NixOS environment
+
+# The adjustment is to add the following line at the top of the script:
+# #!/usr/bin/env bash
+
+echo "Hello World"
