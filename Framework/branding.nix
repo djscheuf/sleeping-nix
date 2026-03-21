8b76528a0c6aea3ee055153d240985b903a97d5b
Daniel Scheufler <djscheufler@gmail.com>
2026-03-21 14:06:15 -0500

    feat(add open dyslexic font)

--- Framework/branding.nix
+++ Framework/branding.nix
@@ -11,6 +11,7 @@
 		arkpandora_ttf #Metrically Identidal to Arial and TNR Fonts
 		roboto
 		libre-baskerville
+		nerd-fonts.open-dyslexic # Added for Dyslexia support
 	];
 
 	fonts.fontDir.enable = true; # should give all programs access to fonts thru a shared fonts folder
