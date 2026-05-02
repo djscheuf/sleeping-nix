dfd8e40ddcab1969051bde08da3a0f64b01fbafe
Daniel Scheufler <djscheufler@gmail.com>
2026-05-01 20:18:39 -0500

    feat(tune os mgmt): optimized nix gc, including free-space based protections.

--- Framework/os-mgmt.nix
+++ Framework/os-mgmt.nix
@@ -1,14 +1,28 @@
-{ configs, pkgs, ... } : 
+{ configs, lib, pkgs, ... } : 
 
 {
 	#setting up automated cron nix GC
 	nix.gc = {
 		automatic = true;
 		dates = "weekly"; # runs weekly
-		options = "--delete-older-than 14d"; # anything older than 2 weeks.
+		options = "--delete-older-than 7d"; # anything older than 1 weeks.
+		persistent = true;
 		# should never delete current generation.
 	};	
 
+	# Limit boot menu entries (choose based on your bootloader)
+    boot.loader.systemd-boot.configurationLimit = 7;
+  	# boot.loader.grub.configurationLimit = 7;
+
+  	# Automatic store optimization (deduplication)
+  	nix.optimise.automatic = true;
+  
+	# Space-based safety net
+	nix.extraOptions = ''
+		min-free = ${toString (3 * 1024 * 1024 * 1024)}      # 3GB threshold
+		max-free = ${toString (10 * 1024 * 1024 * 1024)}  # Free up to 10GB
+	'';
+
 	system.autoUpgrade.enable = true;
-	system.autoUpgrade.channel = "https://channels.nixos.org/nixos-25.05";
+	system.autoUpgrade.channel = "https://channels.nixos.org/nixos-25.11";
 }
